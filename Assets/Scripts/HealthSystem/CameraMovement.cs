b78712c01229cf0e8201f4d28a9c0567a441ced0
Umbrason <Umbrason@Outlook.com>
2025-02-17 22:57:18 +0100

    update player movement & camera

--- Assets/Scripts/HealthSystem/CameraMovement.cs
+++ Assets/Scripts/HealthSystem/CameraMovement.cs
@@ -4,28 +4,14 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private Vector3 offset;
-    [SerializeField] private Transform target;
+    [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private float smoothTime;
-    private Vector3 currentVelocity= Vector3.zero;
+    [SerializeField] private float lookaheadRadius;
+    private Vector3 currentVelocity = Vector3.zero;
 
-    private Quaternion freezeRotation;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-        
-        freezeRotation = transform.rotation;
-        offset = transform.position - target.position;
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.rotation = freezeRotation;
-        Vector3 targetPosition = target.position + offset;
+        var targetPosition = playerMovement.PlayerBody.velocity / playerMovement.Velocity * lookaheadRadius + playerMovement.PlayerBody.position;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
-        
     }
 }
--- Assets/Scripts/HealthSystem/PlayerMovement.cs
+++ Assets/Scripts/HealthSystem/PlayerMovement.cs
@@ -6,36 +6,34 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] bool canRotate = true;
-    NavMeshAgent playerAgent;
-    
-    Rigidbody playerBody;
-    // Start is called before the first frame update
+    [field: SerializeField] public float Velocity { get; private set; }
+    [SerializeField] private float deadzoneVelocityRadius;
+    [SerializeField] private float maxVelocityRadius;
+    [SerializeField] private Transform cameraAnchor;
+    public Rigidbody PlayerBody { get; private set; }
     void Start()
     {
-        playerBody = GetComponent<Rigidbody>();
-        playerAgent = GetComponent<NavMeshAgent>();
-        
+        PlayerBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //Toggle Rotation
         if (!canRotate)
         {
             transform.rotation = Quaternion.identity;
         }
-        
-        if (Input.GetMouseButtonDown(0))
-        {
-           //Use raycast to find position on Navmesh map
-            RaycastHit hit;
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-            {
-                
-                playerAgent.destination = hit.point;
-            }
-        }
-        }
-    
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var t = ray.origin.y / ray.direction.y;
+        var xzIntersection = ray.origin + ray.direction * -t;
+        var direction = xzIntersection - cameraAnchor.position;
+        var mag = direction.magnitude;
+        direction = direction.normalized;
+        mag = Mathf.Clamp(mag - deadzoneVelocityRadius, 0, maxVelocityRadius - deadzoneVelocityRadius);
+        mag /= maxVelocityRadius - deadzoneVelocityRadius;
+        //if (!Input.GetMouseButton(0)) mag = 0; <<< require holding M1
+        PlayerBody.velocity = mag * Velocity * direction;
+    }
+
 }
