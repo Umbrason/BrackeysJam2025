a784087762b6736f8f056c70087fa78b7d24a61f
Heemo2000 <hgupta01062000@gmail.com>
2025-02-18 00:26:26 +0530

    Fixed some issues in Flock Behaviour
    Fixed issues in Flock Behaviour-
    
    1. Agents merging at one place.
    2. Agents having varied rotation.
    3. Agents moving in Y axis.

--- Assets/Scripts/FlockingBehaviourManagement/Behaviour/AvoidanceBehaviour.cs
+++ Assets/Scripts/FlockingBehaviourManagement/Behaviour/AvoidanceBehaviour.cs
@@ -7,11 +7,23 @@ namespace FlockingBehaviourManagement
     [CreateAssetMenu(menuName = "Flock/Behaviour/Avoidance", fileName = "Avoidance")]
     public class AvoidanceBehaviour : FilteredFlockBehaviour
     {
+        public bool affectX = false;
+        public bool affectY = false;
+        public bool affectZ = false;
+
         public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
         {
             //if no neighbors, return no adjustment
             if (context.Count == 0)
+            {
                 return Vector3.zero;
+            }
+
+            Vector3 agentPosition = agent.transform.position;
+
+            if (!affectX) agentPosition.x = 0;
+            if (!affectY) agentPosition.y = 0;
+            if (!affectZ) agentPosition.z = 0;
 
             //add all points together and average
             Vector3 avoidanceMove = Vector3.zero;
@@ -19,12 +31,19 @@ namespace FlockingBehaviourManagement
             List<Transform> filteredContext = (base.contextFilter == null) ? context : base.contextFilter.Filter(agent, context);
             foreach (Transform item in filteredContext)
             {
-                if (Vector3.SqrMagnitude(item.position - agent.transform.position) < flock.SquareAvoidanceRadius)
+                Vector3 itemPosition = item.position;
+
+                if (!affectX) itemPosition.x = 0;
+                if (!affectY) itemPosition.y = 0;
+                if (!affectZ) itemPosition.z = 0;
+
+                if (Vector3.SqrMagnitude(itemPosition - agentPosition) < flock.SquareAvoidanceRadius)
                 {
                     avoidCount++;
                     avoidanceMove += agent.transform.position - item.position;
                 }
             }
+
 
             if (avoidCount > 0)
             {
