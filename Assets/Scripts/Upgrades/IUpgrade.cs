4ef89c76f0c550fc157db705e1e4a73582ce5320
Umbrason <Umbrason@Outlook.com>
2025-02-19 03:34:50 +0100

    Upgrade Names

--- Assets/Scripts/Upgrades/IUpgrade.cs
+++ Assets/Scripts/Upgrades/IUpgrade.cs
@@ -1,13 +1,13 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public interface IUpgrade
 {
     public static LootPool<IUpgrade> UpgradePool;
     Sprite Icon { get; }
+    string Name { get; }
     string Description { get; }
     bool Stackable { get; }
     void OnApply(GameObject PlayerObject);
-    protected void Register() => UpgradePool.Add(this);
+    protected void Register() => UpgradePool.Push(this);
     protected void Unregister() => UpgradePool.Remove(this);
 }
