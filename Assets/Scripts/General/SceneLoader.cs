569232242af1b9405aabd60d6fb86de0e38d641c
Heemo2000 <hgupta01062000@gmail.com>
2025-02-23 02:34:23 +0530

    Added Video functionality.

---
+++ Assets/Scripts/General/SceneLoader.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public UnityEvent OnBeforeLoading;
+    public UnityEvent<float> OnLoading;
+
+    private Coroutine loadingCoroutine = null;
+    public void LoadScene(string sceneName)
+    {
+        if(loadingCoroutine == null)
+        {
+            loadingCoroutine = StartCoroutine(LoadSceneAysnc(sceneName));
+        }
+    }
+
+    private IEnumerator LoadSceneAysnc(string sceneName)
+    {
+        var task = SceneManager.LoadSceneAsync(sceneName);
+
+        OnBeforeLoading?.Invoke();
+
+        while(!task.isDone)
+        {
+            OnLoading?.Invoke(task.progress);
+            yield return null;
+        }
+
+        loadingCoroutine = null;
+
+    }
+}
