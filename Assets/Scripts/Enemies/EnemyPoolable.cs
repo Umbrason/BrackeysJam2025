23e507fec9d46717627e017c2015f73809c195e7
Umbrason <Umbrason@Outlook.com>
2025-02-22 19:53:24 +0100

    VFX works

--- Assets/Scripts/Enemies/EnemyPoolable.cs
+++ Assets/Scripts/Enemies/EnemyPoolable.cs
@@ -8,10 +8,25 @@ public class EnemyPoolable : MonoBehaviour, EnemyPool.IPoolTarget
     Cached<Rigidbody> cached_rb;
     Rigidbody RB => cached_rb[this];
 
+    GameObjectPool HitVFXPool => ((EnemyPool)Owner)?.HitVFXPool;
     GameObjectPool DeathVFXPool => ((EnemyPool)Owner)?.DeathVFXPool;
     GameObjectPool HealthPickupPool => ((EnemyPool)Owner)?.HealthPickupPool;
     [SerializeField] private Vector2Int healthPickupDropRange;
     [SerializeField] private float healthDropRadius;
+
+    Cached<Hitbox> cached_Hitbox = new(Cached<Hitbox>.GetOption.Children);
+    Hitbox Hitbox => cached_Hitbox[this];
+
+    void Awake()
+    {
+        Hitbox.OnDamageEventRegistered += (evt) =>
+        {
+            var pos = transform.position;
+            var srcPos = evt.Source?.transform?.position ?? transform.position + Vector3.up;
+            var delta = pos - srcPos;
+            HitVFXPool.Pull(transform.position, Quaternion.LookRotation(delta));
+        };
+    }
 
     void ObjectPool.IPoolTarget.OnDespawn()
     {
--- Assets/Scripts/HealthSystem/Hitbox.cs
+++ Assets/Scripts/HealthSystem/Hitbox.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Hitbox : MonoBehaviour
@@ -5,8 +6,11 @@ public class Hitbox : MonoBehaviour
     [field: SerializeField] public bool IsCritical { get; private set; }
     Cached<HealthPool> cached_HealthPool = new(Cached<HealthPool>.GetOption.Parent);
     HealthPool HealthPool => cached_HealthPool[this];
+
+    public event Action<HealthEvent> OnDamageEventRegistered;
     public void RegisterDamageEvent(HealthEvent damageEvent)
     {
         HealthPool.RegisterHealthEvent(damageEvent, (IsCritical && damageEvent.CanCrit ? 2 : 1) * (enabled ? 1 : 0));
+        OnDamageEventRegistered?.Invoke(damageEvent);
     }
 }
