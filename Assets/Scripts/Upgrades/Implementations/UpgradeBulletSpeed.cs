eb10d35eec4d78d196b5961c396de8886231ef0d
Werghoul <107267596+Werghoul@users.noreply.github.com>
2025-02-23 02:07:16 +0100

    tweaking enemy

--- Assets/Scripts/Upgrades/Implementations/UpgradeBulletSpeed.cs
+++ Assets/Scripts/Upgrades/Implementations/UpgradeBulletSpeed.cs
@@ -4,7 +4,7 @@ public class UpgradeBulletSpeed : IUpgrade
 {
     public Sprite Icon => null;
     public string Name => "Bullet Speed Up";
-    public string Description => "Increases bullets speed by +1.";
+    public string Description => "Increases bullets speed by +10.";
     public bool Stackable => true;
-    public void OnApply(GameObject PlayerObject) => PlayerObject.GetComponent<PlayerStats>().BulletVelocity.RegisterAdd(1);
+    public void OnApply(GameObject PlayerObject) => PlayerObject.GetComponent<PlayerStats>().BulletVelocity.RegisterAdd(10);
 }
--- Assets/Scripts/Upgrades/Implementations/UpgradeDamageUp.cs
+++ Assets/Scripts/Upgrades/Implementations/UpgradeDamageUp.cs
@@ -4,7 +4,7 @@ public class UpgradeDamageUp : IUpgrade
 {
     public Sprite Icon => null;
     public string Name => "Damage Up";
-    public string Description => "Increases the damage by +5";
+    public string Description => "Increases the damage by +10";
     public bool Stackable => true;
-    public void OnApply(GameObject PlayerObject) => PlayerObject.GetComponent<PlayerStats>().DamagePerBullet.RegisterAdd(5);
+    public void OnApply(GameObject PlayerObject) => PlayerObject.GetComponent<PlayerStats>().DamagePerBullet.RegisterAdd(10);
 }
