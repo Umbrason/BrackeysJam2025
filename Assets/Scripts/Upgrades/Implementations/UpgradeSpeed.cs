5bfdc2fef087dca8c4e491a89e4659be88d60479
Umbrason <Umbrason@Outlook.com>
2025-02-23 04:22:58 +0100

    Update UpgradeSpeed.cs

--- Assets/Scripts/Upgrades/Implementations/UpgradeSpeed.cs
+++ Assets/Scripts/Upgrades/Implementations/UpgradeSpeed.cs
@@ -6,7 +6,7 @@ public class UpgradeSpeed : IUpgrade
 {
 	Sprite IUpgrade.Icon => Resources.Load<Sprite>("MovementSpeedUp");
     string IUpgrade.Name => "TRIPple Boots";
-    string IUpgrade.Description => $"Increases the speed of the player by {increase}.";
+    string IUpgrade.Description => $"Increases the speed of the player by +{increase}.";
     bool IUpgrade.Stackable => true;
     const int increase = 4;
     void IUpgrade.OnApply(GameObject PlayerObject) { PlayerObject.GetComponent<PlayerStats>().Speed.RegisterAdd(increase);}
